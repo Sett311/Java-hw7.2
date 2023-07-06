3f1a6265dfd03f6b08188fd997fb8eb51f2dd627
Sett311 <2004sett@gmal.com>
2023-07-06 21:48:02 +0300

    error fixed

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,5 +1,3 @@
-package resources;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -8,7 +6,7 @@ import ru.netology.javaqa.BonusService;
 public class BonusServiceTest {
 
     @ParameterizedTest()
-    @CsvFileSource(files = "/data.csv")
+    @CsvFileSource(files = "src/test/resources/data.csv")
     void shouldCalculateBonus (int expected, int amount, boolean isRegistered) {
         BonusService service = new BonusService();
         long actual = service.calculate(amount, isRegistered);
